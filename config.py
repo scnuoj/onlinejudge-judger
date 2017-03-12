abf56c1d13fc67a42c0332f25489bb040818f495
Kilerd Chan <blove694@gmail.com>
2017-03-12 23:40:07 +0800

    update the usage of env function

--- config.py
+++ config.py
@@ -1,27 +1,30 @@
+from env import env
+
+
 class ConfigManager(object):
 
     judge_redis = {
-        "host": "127.0.0.1",
-        "port": 6379,
-        "db": 0,
-        "password": None,
-        "namespace": "JUDGE"
+        "host": env("JUDGE_REDIS_HOST", str, "127.0.0.1"),
+        "port": env("JUDGE_REDIS_PORT", int, 6379),
+        "db": env("JUDGE_REDIS_DB", int, 0),
+        "password": env("JUDGE_REDIS_PASSWORD", str, ""),
+        "namespace": env("JUDGE_REDIS_NAMESPACE", str, "JUDGE")
     }
 
     result_redis = {
-        "host": "127.0.0.1",
-        "port": 6379,
-        "db": 0,
-        "password": None,
-        "namespace": "RESULT"
+        "host": env("RESULT_REDIS_HOST", str, "127.0.0.1"),
+        "port": env("RESULT_REDIS_PORT", int, 6379),
+        "db": env("RESULT_REDIS_DB", int, 0),
+        "password": env("RESULT_REDIS_PASSWORD", str, ""),
+        "namespace": env("RESULT_REDIS_NAMESPACE", str, "RESULT")
     }
 
     content_mysql = {
-        "host": "127.0.0.1",
-        "port": 3306,
-        "user": "",
-        "password": "",
-        "database": "judge",
+        "host": env("MYSQL_HOST", str, "127.0.0.1"),
+        "port": env("MYSQL_PORT", int, 3306),
+        "user": env("MYSQL_USER", str, ""),
+        "password": env("MYSQL_PASSWORD", str, ""),
+        "database": env("MYSQL_DATABASE", str, "judge")
     }
 
-    thread_num = 5
+    thread_num = env("THREAD_NUM", int, 5)
--- main.py
+++ main.py
@@ -1,30 +1,9 @@
 from config import ConfigManager
 from task import TaskManager
 from models import MySQLDatabase, database_proxy
-from env import env
+
 my_config = ConfigManager()
-my_config.judge_redis = {
-        "host": env("JUDGE_REDIS_HOST", str, "cloud.ruiming.me"),
-        "port": env("JUDGE_REDIS_PORT", int, 12758),
-        "db": env("JUDGE_REDIS_DB", int, 1),
-        "password": env("JUDGE_REDIS_PASSWORD", str, "NQUZatQ7GM8fzv0ltlQboS3gedz0HlCG"),
-        "namespace": env("JUDGE_REDIS_NAMESPACE", str, "JUDGER")
-    }
-my_config.result_redis = {
-        "host": env("RESULT_REDIS_HOST", str, "cloud.ruiming.me"),
-        "port": env("RESULT_REDIS_PORT", int, 12758),
-        "db": env("RESULT_REDIS_DB", int, 1),
-        "password": env("RESULT_REDIS_PASSWORD", str, "NQUZatQ7GM8fzv0ltlQboS3gedz0HlCG"),
-        "namespace": env("RESULT_REDIS_NAMESPACE", str, "RESULT")
-}
-my_config.content_mysql = {
-        "host": env("MYSQL_HOST", str, "cloud.ruiming.me"),
-        "port": env("MYSQL_PORT", int, 6612),
-        "user": env("MYSQL_USER", str, "scnuoj"),
-        "password": env("MYSQL_PASSWORD", str, "ntsuv2o7x8naq7"),
-        "database": env("MYSQL_DATABASE", str, "onlinejudge")
-    }
-my_config.thread_num = env("THREAD_NUM", int, 5)
+
 database = MySQLDatabase(**my_config.content_mysql)
 
 database_proxy.initialize(database)
